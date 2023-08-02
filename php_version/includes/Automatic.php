b9852e5f99627ef1c485cc2585cc8319e3d3ea5f
Hwangtaehun <101085674+Hwangtaehun@users.noreply.github.com>
2023-08-02 21:35:52 +0900

    순서 변경

--- php_version/includes/Automatic.php
+++ php_version/includes/Automatic.php
@@ -7,17 +7,17 @@ class Automatic {
 	public function __construct(PDO $pdo) {
 		$this->pdo = $pdo;
 		$this->str_today = date("Y-m-d");
-		$this->overdue_manager();
 		$this->clearmember();
+		$this->overdue_manager();
 	}
 	
 	private function overdue_manager() {
 		$this->sql = "SELECT * FROM lent WHERE len_re_date is NULL";
-		echo "overdue_manager에 sql값: $this->sql<br>";
+		//echo "overdue_manager에 sql값: $this->sql<br>";
 		$result = $this->pdo->query($this->sql);
 		$num = $result->rowCount();
 		$count = 0;
-		echo '$num = '.$num.', $count = '.$count;
+		//echo '$num = '.$num.', $count = '.$count;
 
 		if($num != 0) {	
 			try {
@@ -25,27 +25,27 @@ class Automatic {
 					$str_estimate_date = $this->estimateReturndate($row->len_date, $row->len_re_st);
 					$estimate_date = strtotime($str_estimate_date);
 					$today = strtotime($this->str_today);
-					echo "\$estimate_date: $estimate_date, \$today: $today";
-					echo '<br>';
+					//echo "\$estimate_date: $estimate_date, \$today: $today";
+					//echo '<br>';
 					if($estimate_date < $today ) {
 						$len_no_array[$count] = $row->len_no;
-						echo '$len_no_array['.$count.'] = '.$len_no_array[$count].'<br>';
+						//echo '$len_no_array['.$count.'] = '.$len_no_array[$count].'<br>';
 						$count++;
 						$this->sql = "UPDATE member SET mem_state = 2 WHERE mem_no = $row->mem_no";
-						echo '$sql: '.$this->sql.'데이터타입: '.gettype($this->sql);
+						//echo '$sql: '.$this->sql.'데이터타입: '.gettype($this->sql);
 						$this->pdo->exec($this->sql);
 					}
 				}
 
 				for($i = 0; $i < $count; $i++) {
 					$this->sql = "SELECT * FROM overdue WHERE len_no = $len_no_array[$i]";
-					echo '$sql: '.$this->sql.'<br>';
+					//echo '$sql: '.$this->sql.'<br>';
 					$result = $this->pdo->query($this->sql);
 					$num = $result->rowCount();
 
 					if($num == 0){
 						$this->sql = "INSERT INTO overdue SET len_no = $len_no_array[$i]";
-						echo '$sql: '.$this->sql.'<br>';
+						//echo '$sql: '.$this->sql.'<br>';
 						$this->pdo->exec($this->sql);
 					}
 				}
@@ -58,7 +58,7 @@ class Automatic {
 	
 	private function clearmember() {
 		$this->sql = "SELECT * FROM lent INNER JOIN overdue ON lent.len_no = overdue.len_no WHERE overdue.due_exp <= '$this->str_today'";
-		echo '$sql: '.$this->sql.'<br>';
+		//echo '$sql: '.$this->sql.'<br>';
 		$result = $this->pdo->query($this->sql);
 		$result->setFetchMode(PDO::FETCH_NUM);
 		$num = $result->rowCount();
@@ -82,10 +82,10 @@ class Automatic {
 
 				for($i = 0; $i < $num; $i++) {
 					$this->sql = "UPDATE member SET mem_state = 0 WHERE mem_no = $mem_no[$i]";
-					echo 'sql의 값: '.$this->sql;
+					//echo 'sql의 값: '.$this->sql;
 					$this->pdo->exec($this->sql);
 					$this->sql = "DELETE FROM overdue WHERE due_no = $due_no[$i]";
-					echo 'sql의 값: '.$this->sql;
+					//echo 'sql의 값: '.$this->sql;
 					$this->pdo->exec($this->sql);
 				}
 			} catch(PDOException $e){
