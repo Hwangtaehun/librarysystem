22808b4294786a48c5ac668a658e4caca73d89fb
Hwangtaehun <r283@naver.com>
2023-08-03 21:08:04 +0900

    Update BookSymbol.php

--- php_version/includes/BookSymbol.php
+++ php_version/includes/BookSymbol.php
@@ -4,77 +4,72 @@ class BookSymbol{
 	private $joong = array("ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ");
 	private $jong = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
 	
-	private $author_array = [];
     private $author_symbol;
-	private $author_char_array = [];
 	private $lastauthor_exist;
 	
-    public function __construct(String $author, boolean $bool){
-        $lastauthor_exist = bool;
-		$author_array = $author.split(" ");
-		$this->inital();
+    public function __construct(String $author, bool $bool){
+        $this->lastauthor_exist = $bool;
+		$author_array = mb_str_split($author, $split_length = 1, $encoding = "utf-8");
+		$this->inital($author_array);
     }
 	
-	private function inital() {
+	private function inital(array $author_array) {
 		$num = 0;
+		$author_char_array = [];
 		
-		while(english_check(author_array[$num])) {
-			if(sizeof(author_array)-1 == $num) {
+		while($this->english_check($author_array[$num])) {
+			if(sizeof($author_array)-1 == $num) {
 				break;
 			}
 			$num++;
 		}
 		
 		if($num > 0) {
-			if(korean_check(author_array[$num])) {
-				author_char_array = stringTochar(author_array[$num]);
+			if($this->korean_check($author_array[$num])) {
+				$author_char_array = $this->stringTochar($author_array[$num]);
 			}
 			else {
-				if(lastauthor_exist) {
-					author_array = sequence_change(author_array);
+				if($this->lastauthor_exist) {
+					$author_array = $this->sequence_change($author_array);
 				}
-				author_array = englishTokorean(author_array[0]);
-				author_char_array = first_word(author_array);
+				$author_array = $this->englishTokorean($author_array[0]);
+				$author_char_array = $this->first_word($author_array);
 			}
 		}
-		else if(author_array.length > 1) {
-			if(lastauthor_exist) {
-				author_array = sequence_change(author_array);
+		else if(sizeof($author_array) > 1) {
+			if($this->lastauthor_exist) {
+				$author_array = $this->sequence_change($author_array);
 			}
-			author_char_array = stringTochar(author_array[0]);
+			$author_char_array = $this->stringTochar($author_array[0]);
 		}
 		else {
-			if(korean_check(author_array[0])) {
-				author_char_array = stringTochar(author_array[0]);
+			if($this->korean_check($author_array[0])) {
+				$author_char_array = $this->stringTochar($author_array[0]);
 			}
 		}
 		
-		if(author_char_array != null) {
-			finish_symbol();
+		if($author_char_array != null) {
+			$this->finish_symbol($author_char_array);
 		}
 	}
 	
 	private function korean_check(String $str) {
-		return Pattern.matches("^[ㄱ-ㅎ가-힣]*$", $str);
+		return preg_match("^[ㄱ-ㅎ가-힣]*$", $str);
 	}
 	
 	private function english_check(String $str) {
-		$character = [];
+		$character = explode( '.', $str );
 		
-		character = $str.split(".");
-		
-		if(character.length > 0) {
-			return Pattern.matches("^[a-zA-Z]*$", $character[0]);
+		if(sizeof($character) > 0) {
+			return preg_match("^[a-zA-Z]*$", $character[0]);
 		}
 		else {
-			return Pattern.matches("^[a-zA-Z]*$", $str);
+			return preg_match("^[a-zA-Z]*$", $str);
 		}
 	}
 	
-	private function sequence_change(String $str) {
-		$temp;
-		
-		$temp = $str[str.length-1];
+	private function sequence_change(array $str) {
+		$temp = $str[sizeof($str)];
 		$str[sizeof($str)-1] = $str[0];
 		$str[0] = $temp;
 		
@@ -84,17 +79,18 @@ class BookSymbol{
 	private function englishTokorean(String $str) {
 		$result = "";
 		$result_array = [];
+		$array = explode( '', $str );
 		
-		for($i = 0; $i < sizeof($str); $i++) {
-			switch(str.charAt($i)) {
+		for($i = 0; $i < sizeof($array); $i++) {
+			switch($array[$i]) {
 			case 'a':
 			case 'A':
-				if($i == sizeof($str) - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅏ";
 				}
 				else {
 					$i++;
-					if(str.charAt($i) == 'e') {
+					if($array[$i] == 'e') {
 						$result += "ㅐ";
 					}
 					else {
@@ -109,12 +105,12 @@ class BookSymbol{
 				break;
 			case 'c':
 			case 'C':
-				if($i == sizeof($str) - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅋ";
 				}
 				else {
 					$i++;
-					if(str.charAt($i) == 'h') {
+					if($array[$i] == 'h') {
 						$result += "ㅊ";
 					}
 					else {
@@ -129,18 +125,18 @@ class BookSymbol{
 				break;
 			case 'e':
 			case 'E':
-				if(i == sizeof($str) - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅔ";
 				}
 				else {
 					$i++;
-					if(str.charAt($i) == 'o') {
+					if($array[$i] == 'o') {
 						$result += "ㅓ";
 					}
-					else if(str.charAt($i) == 'u') {
+					else if($array[$i] == 'u') {
 						$result += "ㅡ";
 					}
-					else if(str.charAt($i) == 'e') {
+					else if($array[$i] == 'e') {
 						$result += "ㅣ";
 					}
 					else {
@@ -167,12 +163,12 @@ class BookSymbol{
 				break;
 			case 'J':
 			case 'j':
-				if($i == str.length() - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅈ";
 				}
 				else {
 					$i++;
-					if(str.charAt(i) == 'j') {
+					if($array[$i] == 'j') {
 						$result += "ㅉ";
 					}
 					else {
@@ -183,12 +179,12 @@ class BookSymbol{
 				break;
 			case 'K':
 			case 'k':
-				if($i == str.length() - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅋ";
 				}
 				else {
 					$i++;
-					if(str.charAt(i) == 'k') {
+					if($array[$i] == 'k') {
 						$result += "ㄲ";
 					}
 					else {
@@ -207,12 +203,12 @@ class BookSymbol{
 				break;
 			case 'N':
 			case 'n':
-				if($i == sizeof($str) - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㄴ";
 				}
 				else {
 					$i++;
-					if(str.charAt(i) == 'g') {
+					if($array[$i] == 'g') {
 						$result += "ㅇ";
 					}
 					else {
@@ -223,15 +219,15 @@ class BookSymbol{
 				break;
 			case 'O':
 			case 'o':
-				if($i == sizeof($str) - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅗ";
 				}
 				else {
 					$i++;
-					if(str.charAt(i) == 'e') {
+					if($array[$i] == 'e') {
 						$result += "ㅚ";
 					}
-					else if(str.charAt(i) == 'o') {
+					else if($array[$i] == 'o') {
 						$result += "ㅜ";
 					}
 					else {
@@ -242,12 +238,12 @@ class BookSymbol{
 				break;
 			case 'P':
 			case 'p':
-				if($i == sizeof($str) - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅍ";
 				}
 				else {
 					$i++;
-					if(str.charAt(i) == 'p') {
+					if($array[$i] == 'p') {
 						$result += "ㅃ";
 					}
 					else {
@@ -265,49 +261,49 @@ class BookSymbol{
 				$result += "ㄹ";
 				break;
 			case 'S':
-			case 's'://여기까지
-				if(i == str.length() - 1) {
+			case 's':
+				if($i == sizeof($array) - 1) {
 					$result += "ㅅ";
 				}
 				else {
-					i++;
-					if(str.charAt(i) == 's') {
+					$i++;
+					if($array[$i] == 's') {
 						$result += "ㅆ";
 					}
 					else {
-						i--;
+						$i--;
 						$result += "ㅅ";
 					}
 				}
 				break;
 			case 'T':
 			case 't':
-				if(i == str.length() - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅌ";
 				}
 				else {
-					i++;
-					if(str.charAt(i) == 't') {
+					$i++;
+					if($array[$i] == 't') {
 						$result += "ㄸ";
 					}
 					else {
-						i--;
+						$i--;
 						$result += "ㅌ";
 					}
 				}
 				break;
 			case 'U':
 			case 'u':
-				if(i == str.length() - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅜ";
 				}
 				else {
-					i++;
-					if(str.charAt(i) == 'i') {
+					$i++;
+					if($array[$i] == 'i') {
 						$result += "ㅢ";
 					}
 					else {
-						i--;
+						$i--;
 						$result += "ㅜ";
 					}
 				}
@@ -318,37 +314,37 @@ class BookSymbol{
 				break;
 			case 'W':
 			case 'w':
-				if(i == str.length() - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅝ";
 				}
 				else {
-					i++;
-					if(str.charAt(i) == 'a') {
-						if(i == str.length() - 1) {
+					$i++;
+					if($array[$i] == 'a') {
+						if($i == sizeof($array) - 1) {
 							$result += "ㅘ";
 						}
 						else {
-							i++;
-							if(str.charAt(i) == 'e') {
+							$i++;
+							if($array[$i] == 'e') {
 								$result += "ㅙ";
 							}
 							else {
-								i--;
+								$i--;
 								$result += "ㅘ";
 							}
 						}
 					}
-					else if(str.charAt(i) == 'e') {
+					else if($array[$i] == 'e') {
 						$result += "ㅞ";
 					}
-					else if(str.charAt(i) == 'i') {
+					else if($array[$i] == 'i') {
 						$result += "ㅟ";
 					}
-					else if(str.charAt(i) == 'o') {
+					else if($array[$i] == 'o') {
 						$result += "ㅝ";
 					}
 					else {
-						i--;
+						$i--;
 						$result += "ㅝ";
 					}
 				}
@@ -362,51 +358,51 @@ class BookSymbol{
 				break;
 			case 'Y':
 			case 'y':
-				if(i == str.length() - 1) {
+				if($i == sizeof($array) - 1) {
 					$result += "ㅏ";
 					$result += "ㅇ";
 					$result += "ㅣ";
 				}
 				else {
-					i++;
-					if(str.charAt(i) == 'a') {
-						if(i == str.length() - 1) {
+					$i++;
+					if($array[$i] == 'a') {
+						if($i == sizeof($array) - 1) {
 							$result += "ㅒ";
 						}
 						else {
-							i++;
-							if(str.charAt(i) == 'e') {
+							$i++;
+							if($array[$i] == 'e') {
 								$result += "ㅑ";
 							}
 							else {
-								i--;
+								$i--;
 								$result += "ㅒ";
 							}
 						}
 					}
-					else if(str.charAt(i) == 'e') {
-						if(i == str.length() - 1) {
+					else if($array[$i] == 'e') {
+						if($i == sizeof($array) - 1) {
 							$result += "ㅖ";
 						}
 						else {
-							i++;
-							if(str.charAt(i) == 'o') {
+							$i++;
+							if($array[$i] == 'o') {
 								$result += "ㅕ";
 							}
 							else {
-								i--;
+								$i--;
 								$result += "ㅖ";
 							}
 						}
 					}
-					else if(str.charAt(i) == 'o') {
+					else if($array[$i] == 'o') {
 						$result += "ㅛ";
 					}
-					else if(str.charAt(i) == 'u') {
+					else if($array[$i] == 'u') {
 						$result += "ㅠ";
 					}
 					else {
-						i--;
+						$i--;
 						$result += "ㅣ";
 					}
 				}
@@ -417,178 +413,164 @@ class BookSymbol{
 				break;
 			}
 		}
-		System.out.println("englishTokorean함수의 $result: " + $result);
-		$result_array = $result.split("");
+		//echo 'englishTokorean함수의 $result: '.$result;
+		$result_array = explode('', $result);
 		
 		return $result_array;
 	}
 	
-	private char[] first_word(String[] str) {
-		int $num = 0, cho_$num = 99, joong_$num = 99, jong_$num = 0;
-		String $result;
-		char unicode, $result_array[] = null;
+	private function first_word(array $str) {
+		$num = 0;
+		$cho_num = 99;
+		$joong_num = 99;
+		$jong_num = 0;
+		$result_array = [];
 		
-		for(int i = 0; i < cho.length; i++) {
-			if(str[$num].equals(cho[i])) {
-				cho_$num = i;
+		for($i = 0; $i < sizeof($this->cho); $i++) {
+			if($str[$num] === $this->cho[$i]) {
+				$cho_num = $i;
 			}
 		}
 		
-		if(cho_$num == 99) {
-			cho_$num = 11;
+		if($cho_num == 99) {
+			$cho_num = 11;
 		}
-		
-		System.out.println("cho_$num: " + cho_$num);
+		//echo 'cho_num: '.$cho_num;
 		
 		$num++;
 		
-		for(int i = 0; i < joong.length; i++) {
-			if(str[$num].equals(joong[i])) {
-				joong_$num = i;
+		for($i = 0; $i < sizeof($this->joong); $i++) {
+			if($str[$num] === $this->joong[$i]) {
+				$joong_num = $i;
 			}
 		}
-		
-		System.out.println("joong_$num: " + joong_$num);
+		//echo 'joong_num: '.$joong_num;
 		
 		$num++;
 		
-		for(int i = 0; i < jong.length; i++) {
-			if(str[$num].equals(jong[i])) {
-				jong_$num = i;
+		for($i = 0; $i < sizeof($this->jong); $i++) {
+			if($str[$num] === $this->jong[$i]) {
+				$jong_num = $i;
 			}
 		}
+		//echo 'jong_num: '.$jong_num;
 		
-		System.out.println("jong_$num: " + jong_$num);
-		
-		if(jong_$num == 0) {
+		if($jong_num == 0) {
 			$num--;
 		}
 		
-		if(joong_$num == 99) {
+		if($joong_num == 99) {
 			return $result_array;
 		}
 		
 		$num++;
 		
-		unicode = (char)((cho_$num * 21 + joong_$num) * 28 + jong_$num + 0xAC00);
-		System.out.println("unicode = " + unicode);
+		$unicode = ($cho_num * 21 + $joong_num) * 28 + $jong_num + 0xAC00;
+		//echo 'unicode = '.$unicode;
 		
-		$result = String.valueOf(unicode);
-		for(int i = $num; i < str.length; i++) {
-			$result += str[i];
+		$result = $unicode;
+		for($i = $num; $i < sizeof($str); $i++) {
+			$result += $str[$i];
 		}
 		
-		$result_array = new char[$result.length()];
+		$result_array = mb_str_split($result, $split_length = 1, $encoding = "utf-8");
+		return $result_array;
+	}
+	
+	private function stringTochar(String $str) {
+		$array = mb_str_split($str, $split_length = 1, $encoding = "utf-8");
+		$result_array = [];
 		
-		for(int i = 0; i < $result.length(); i++) {
-			$result_array[i] = $result.charAt(i);
-		}
+		$str_result = $array[0];
+		$uniVal = $array[1];
+		$str_result += $this->separate_character($uniVal);
+		$result_array = mb_str_split($str_result, $split_length = 1, $encoding = "utf-8");
 		
 		return $result_array;
 	}
 	
-	private char[] stringTochar(String str) {
-		String str_$result;
-		char uniVal, $result_array[];
-		
-		str_$result = String.valueOf(str.charAt(0));
-		uniVal = str.charAt(1);
-		
-		str_$result += separate_character(uniVal);
-		
-		$result_array = new char[str_$result.length()];
-		
-		for(int i = 0; i < str_$result.length(); i++) {
-			$result_array[i] = str_$result.charAt(i);
-		}
-		
-		return $result_array;
-	}
-	
-	private void finish_symbol() {
-		int $num = 0;
-		
-		author_symbol = String.valueOf(author_char_array[$num]);
-		
+	private function finish_symbol(array $author_char_array) {
+		$num = 0;
+		$this->author_symbol = $author_char_array[$num];
 		$num++;
 		
-		switch(author_char_array[$num]) {
+		switch($author_char_array[$num]) {
 		case 'ㄱ':
 		case 'ㄲ':
-			author_symbol += "1";
+			$this->author_symbol += "1";
 			break;
 		case 'ㄴ':
-			author_symbol += "19";
+			$this->author_symbol += "19";
 			break;
 		case 'ㄷ':
 		case 'ㄸ':
-			author_symbol += "2";
+			$this->author_symbol += "2";
 			break;
 		case 'ㄹ':
-			author_symbol += "29";
+			$this->author_symbol += "29";
 			break;
 		case 'ㅁ':
-			author_symbol += "3";
+			$this->author_symbol += "3";
 			break;
 		case 'ㅂ':
 		case 'ㅃ':
-			author_symbol += "4";
+			$this->author_symbol += "4";
 			break;
 		case 'ㅅ':
 		case 'ㅆ':
-			author_symbol += "5";
+			$this->author_symbol += "5";
 			break;
 		case 'ㅇ':
-			author_symbol += "6";
+			$this->author_symbol += "6";
 			break;
 		case 'ㅈ':
 		case 'ㅉ':
-			author_symbol += "7";
+			$this->author_symbol += "7";
 			break;
 		case 'ㅊ':
-			author_symbol += "8";
+			$this->author_symbol += "8";
 			break;
 		case 'ㅋ':
-			author_symbol += "87";
+			$this->author_symbol += "87";
 			break;
 		case 'ㅌ':
-			author_symbol += "88";
+			$this->author_symbol += "88";
 			break;
 		case 'ㅍ':
-			author_symbol += "89";
+			$this->author_symbol += "89";
 			break;
 		case 'ㅎ':
-			author_symbol += "9";
+			$this->author_symbol += "9";
 			break;
 		}
 		
-		if(author_symbol.equals(String.valueOf(author_char_array[0]))) {
-			author_symbol += "6";
+		if($this->author_symbol == $author_char_array[0]) {
+			$this->author_symbol += "6";
 		}
 		else {
 			$num++;
 		}
 		
-		if(author_char_array[$num-1] == 'ㅊ') {
-			switch(author_char_array[$num]) {
+		if($author_char_array[$num-1] == 'ㅊ') {
+			switch($author_char_array[$num]) {
 			case 'ㅏ':
 			case 'ㅐ':
 			case 'ㅑ':
 			case 'ㅒ':
-				author_symbol += "2";
+				$this->author_symbol += "2";
 				break;
 			case 'ㅓ':
 			case 'ㅔ':
 			case 'ㅕ':
 			case 'ㅖ':
-				author_symbol += "3";
+				$this->author_symbol += "3";
 				break;
 			case 'ㅗ':
 			case 'ㅘ':
 			case 'ㅙ':
 			case 'ㅚ':
 			case 'ㅛ':
-				author_symbol += "4";
+				$this->author_symbol += "4";
 				break;
 			case 'ㅜ':
 			case 'ㅝ':
@@ -597,72 +579,71 @@ class BookSymbol{
 			case 'ㅠ':
 			case 'ㅡ':
 			case 'ㅢ':
-				author_symbol += "5";
+				$this->author_symbol += "5";
 				break;
 			case 'ㅣ':
-				author_symbol += "6";
+				$this->author_symbol += "6";
 				break;
 			}
 		}
 		else {
-			switch(author_char_array[$num]) {
+			switch($author_char_array[$num]) {
 			case 'ㅏ':
-				author_symbol += "2";
+				$this->author_symbol += "2";
 				break;
 			case 'ㅐ':
 			case 'ㅑ':
 			case 'ㅒ':
-				author_symbol += "3";
+				$this->author_symbol += "3";
 				break;
 			case 'ㅓ':
 			case 'ㅔ':
 			case 'ㅕ':
 			case 'ㅖ':
-				author_symbol += "4";
+				$this->author_symbol += "4";
 				break;
 			case 'ㅗ':
 			case 'ㅘ':
 			case 'ㅙ':
 			case 'ㅚ':
 			case 'ㅛ':
-				author_symbol += "5";
+				$this->author_symbol += "5";
 				break;
 			case 'ㅜ':
 			case 'ㅝ':
 			case 'ㅞ':
 			case 'ㅟ':
 			case 'ㅠ':
-				author_symbol += "6";
+				$this->author_symbol += "6";
 				break;
 			case 'ㅡ':
 			case 'ㅢ':
-				author_symbol += "7";
+				$this->author_symbol += "7";
 				break;	
 			case 'ㅣ':
-				author_symbol += "8";
+				$this->author_symbol += "8";
 				break;
 			}
 		}
 	}
 	
-	public String separate_character(char uniVal) {
-		String $result = "";
-		int $num_cho, $num_joong, $num_jong;
+	public function separate_character($uniVal) {
+		$result = "";
 		
-		$num_cho = (uniVal-0xAC00)/28/21;
-		$num_joong = (uniVal - 0xAC00)/28%21;
-		$num_jong = (uniVal - 0xAC00)%28;
+		$num_cho = ($uniVal-0xAC00)/28/21;
+		$num_joong = ($uniVal - 0xAC00)/28%21;
+		$num_jong = ($uniVal - 0xAC00)%28;
 		
-		$result += cho[$num_cho];
-		$result += joong[$num_joong];
-		$result += jong[$num_jong];
-		$result.trim();
+		$result += $this->cho[$num_cho];
+		$result += $this->joong[$num_joong];
+		$result += $this->jong[$num_jong];
+		trim($result);
 		
 		return $result;
 	}
 	
-	public String call_symbol() {
-		return author_symbol;
+	public function call_symbol() {
+		return $this->author_symbol;
 	}
 }
 
