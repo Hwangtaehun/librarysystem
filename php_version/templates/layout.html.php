6e1102037ef406a9d4f780c3aa88557997c7c21e
Hwangtaehun <r283@naver.com>
2023-08-09 22:03:28 +0900

    수정
    메뉴 드롭다운으로 변경

--- php_version/templates/layout.html.php
+++ php_version/templates/layout.html.php
@@ -13,22 +13,72 @@
             <ul>
                <?php
                $state = $_SESSION['mem_state'];
-               echo '<li><a href="/">홈</a><li>';
+               echo '<li><a href="/">홈</a></li>';
                if($state == 1){
-                echo '<li><a href="php/book.php">책</a><li>';
-                echo '<li><a href="php/kind.php">종류</a><li>';
-                echo '<li><a href="php/library.php">도서관</a><li>';
-                echo '<li><a href="php/material.php">자료</a><li>';
-                echo '<li><a href="/member/list">회원</a><li>';
-                echo '<li><a href="php/lent.php">대출 및 반납</a><li>';
-                echo '<li><a href="php/delivery.php">상호대차</a><li>';
-                echo '<li><a href="php/etc.php">기타</a><li>';
+                echo '<li><a>책</a>';
+                echo '<ul class="submenu">';
+                echo '<li><a href="#">책관리</a></li>';
+                echo '<li><a href="#">책추가</a></li>';
+                echo '</ul>';
+                echo '</li>';
+                echo '<li><a>종류</a>';
+                echo '<ul class="submenu">';
+                echo '<li><a href="#">종류관리</a></li>';
+                echo '<li><a href="#">종류추가</a></li>';
+                echo '</ul>';
+                echo '</li>';
+                echo '<li><a>도서관</a>';
+                echo '<ul class="submenu">';
+                echo '<li><a href="#">도서관관리</a></li>';
+                echo '<li><a href="#">도서관추가</a></li>';
+                echo '</ul>';
+                echo '</li>';
+                echo '<li><a>자료</a>';
+                echo '<ul class="submenu">';
+                echo '<li><a href="#">자료관리</a></li>';
+                echo '<li><a href="#">자료추가</a></li>';
+                echo '</ul>';
+                echo '</li>';
+                echo '<li><a href="/member/list">회원</a></li>';
+                echo '<li><a>대출 및 반납</a>';
+                echo '<ul class="submenu">';
+                echo '<li><a href="#">대출관리</a></li>';
+                echo '<li><a href="#">대출추가</a></li>';
+                echo '<li><a href="#">반납추가</a></li>';
+                echo '</ul>';
+                echo '</li>';
+                echo '<li><a>상호대차</a>';
+                echo '<ul class="submenu">';
+                echo '<li><a href="#">상호대차관리</a></li>';
+                echo '<li><a href="#">상호대차도착일추가</a></li>';
+                echo '<li><a href="#">상호대차완료내역</a></li>';
+                echo '</ul>';
+                echo '</li>';
+                echo '<li><a>기타</a></li>';
+                echo '<ul class="submenu">';
+                echo '<li><a href="#">예약관리</a></li>';
+                echo '<li><a href="#">대출장소관리</a></li>';
+                echo '<li><a href="#">연체관리</a></li>';
+                echo '</ul>';
+                echo '</li>';
                }
                else{
                 $mem_no = $_SESSION['mem_no'];
-                echo '<li><a href="php/material.php">자료 검색</a><li>';
-                echo '<li><a href="php/book.php">내서재</a><li>';
-                echo '<li><a href="/member/addupdate?mem_no='.$mem_no.'">마이페이지</a><li>';
+                echo '<li><a href="php/material.php">자료 검색</a></li>';
+                echo '<li><a>내서재</a></li>';
+                echo '<ul class="submenu">';
+                echo '<li><a href="#">대출중도서</a></li>';
+                echo '<li><a href="#">모든대출내역</a></li>';
+                echo '<li><a href="#">예약내역</a></li>';
+                echo '<li><a href="#">상호대차내역</a></li>';
+                echo '</ul>';
+                echo '</li>';
+                echo '<li><a>마이페이지</a>';
+                echo '<ul class="submenu">';
+                echo '<li><a href="/member/addupdate?mem_no='.$mem_no.'">회원정보수정</a></li>';
+                echo '<li><a href="#">회원탈퇴</a></li>';
+                echo '</ul>';
+                echo '</li>';
                }
                echo '<li><a href="/member/logout">로그아웃</a></li>';
                ?>
