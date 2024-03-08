3c88135e5b3390113979335fd4865bf90ae37aa0
Hwangtaehun <r283@naver.com>
2024-03-08 11:58:39 +0900

    업로드

--- next-version/app/page.tsx
+++ next-version/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Index(){
+import PDO from "../components/Dbconnect"
+
+export default async function Index(){
+    const sql = 'select * from book'
+    const data = await PDO(sql, '')
+    const getdata = JSON.parse(JSON.stringify(data))
+    console.log(getdata)
     return <h1>Hello NextJs</h1>
 }
---
+++ next-version/components/Dbconnect.tsx
@@ -0,0 +1,33 @@
+import { createPool } from 'mysql2'
+
+const pool = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: 3306,
+})
+
+pool.getConnection((err, conn) => {
+    if (err) console.log('fail connected')
+    else console.log('Connected')
+    conn.release()
+})
+
+const PDO = (sql: string, arrParams) => {
+    return new Promise((resolve, reject) => {
+        try {
+            pool.query(sql, arrParams, (err, data) => {
+                if (err) {
+                    console.log('Error in executing the query')
+                    reject(err)
+                }
+                resolve(data)
+            })
+        } catch (err) {
+            reject(err)
+        }
+    })
+}
+
+export default PDO
