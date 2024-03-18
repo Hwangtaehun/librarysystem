f71ccca0dc28d0940803b5dd295d3ef3d13d6e0d
Hwangtaehun <r283@naver.com>
2024-03-18 12:01:28 +0900

    업로드

--- next-version/components/includes/BookSymbol.tsx
+++ next-version/components/includes/BookSymbol.tsx
@@ -54,7 +54,7 @@ class BookSymbol{
 				$author_char_array = $this->stringTochar($author_array[$num]);
 			}
 			else {
-				$author_array = $this->englishTokorean($this->author);
+				//$author_array = $this->englishTokorean($this->author);
 				if($this->lastauthor_exist) {
 					$author_array = $this->sequence_change($author_array);
 				}
@@ -106,7 +106,7 @@ class BookSymbol{
 		return str;
 	}
 	
-	//영어 -> 한글로 변경(확인)
+	//영어 -> 한글로 변경(나중 고치기)
 	private englishTokorean(str: string) {	
         const URL = "https://dapi.kakao.com/v2/search/web";
         const HEADERS = {
@@ -124,42 +124,42 @@ class BookSymbol{
         }
         });
 
-		$parameter = array(
-			"query"=>$str
-		);
+		// $parameter = array(
+		// 	"query"=>$str
+		// );
 		 
-		$host = "https://dapi.kakao.com/v2/search/web";
-		$api_key = "4a10552ea42f3e71bb5c39c7994f8602";
-		$headers = array("Authorization: KakaoAK {$api_key}");
+		// $host = "https://dapi.kakao.com/v2/search/web";
+		// $api_key = "4a10552ea42f3e71bb5c39c7994f8602";
+		// $headers = array("Authorization: KakaoAK {$api_key}");
 		 
-		$query = http_build_query($parameter);
-		$content_type = "json";
+		// $query = http_build_query($parameter);
+		// $content_type = "json";
 		 
-		$opts = array(
-			CURLOPT_URL => $host . '.' . $content_type . '?' . $query,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_SSL_VERIFYPEER => false,
-			CURLOPT_SSLVERSION => 1,
-			CURLOPT_HEADER => false,
-			CURLOPT_HTTPHEADER => $headers
-		);
+		// $opts = array(
+		// 	CURLOPT_URL => $host . '.' . $content_type . '?' . $query,
+		// 	CURLOPT_RETURNTRANSFER => true,
+		// 	CURLOPT_SSL_VERIFYPEER => false,
+		// 	CURLOPT_SSLVERSION => 1,
+		// 	CURLOPT_HEADER => false,
+		// 	CURLOPT_HTTPHEADER => $headers
+		// );
 		 
-		$curl_session = curl_init();
-		curl_setopt_array($curl_session, $opts);
-		$return_data = curl_exec($curl_session);
+		// $curl_session = curl_init();
+		// curl_setopt_array($curl_session, $opts);
+		// $return_data = curl_exec($curl_session);
 		
-		$decode = json_decode($return_data, true);
-		$result = $decode['documents']['0']['title'];
-		$result = explode("-", $result);
-		$result_array = rtrim($result[0]);
-		$result_array = explode(" ", $result_array);
+		// $decode = json_decode($return_data, true);
+		// $result = $decode['documents']['0']['title'];
+		// $result = explode("-", $result);
+		// $result_array = rtrim($result[0]);
+		// $result_array = explode(" ", $result_array);
 		 
-		if (curl_errno($curl_session)) {
-			throw new Exception(curl_error($curl_session));
-		} else {
-			curl_close($curl_session);
-		}
-		return $result_array;
+		// if (curl_errno($curl_session)) {
+		// 	throw new Exception(curl_error($curl_session));
+		// } else {
+		// 	curl_close($curl_session);
+		// }
+		// return $result_array;
 	}
 	//출처: https://mintea.tistory.com/7
 	
