674fea09d9f7d28476f7faccd5a87f15f26e9a14
Hwangtaehun <r283@naver.com>
2024-03-25 21:58:29 +0900

    Controller.tsx를 완성

--- next-version/app/page.tsx
+++ next-version/app/page.tsx
@@ -1,15 +1,12 @@
-"use client";
-
-import { useSearchParams } from "next/navigation"
 import PDO from "../components/includes/Dbconnect"
 
 export default async function Index(){
-    const sql = 'select * from book'
-    const data = await PDO(sql, '')
-    const getdata: string[] = JSON.parse(JSON.stringify(data))
+    // const sql = 'select * from book'
+    // const data = await PDO(sql, '')
+    // const getdata: string[] = JSON.parse(JSON.stringify(data))
     // console.log(getdata)
     // var string = getdata[1]['book_name'] + getdata[1]['book_year']
-    console.log(getdata.length);
+    // console.log(getdata.length);
     // getdata.forEach(function (row) {
     //     console.log(row['book_name']);
     // });
@@ -17,13 +14,13 @@ export default async function Index(){
     // const insert_sql = "INSERT INTO `library` SET `lib_name` = '청주가로수도서관', `lib_date` = '2021-04-29', `lib_zip` = '28383', `lib_add` = '충청북도 청주시 흥덕구 서현서로 5'";
     // PDO(insert_sql, '');
     // console.log('success');
-    const str = useSearchParams();
-    if(str.get('name') != null){
-        var name = str.get('name');
-        console.log();
-    }else{
-        console.log('nothing');
-    }
+    // const str = useSearchParams();
+    // if(str.get('name') != null){
+    //     var name = str.get('name');
+    //     console.log();
+    // }else{
+    //     console.log('nothing');
+    // }
 
     return <h1>Hello NextJs</h1>
 }
--- next-version/components/Controller.tsx
+++ next-version/components/Controller.tsx
@@ -1,4 +1,5 @@
 import { Assistance } from "./includes/Assistance";
+import PDO from "./includes/Dbconnect";
 
 export class Controller{
     private libTable: string[] = ['library', 'lib_no'];
@@ -14,47 +15,48 @@ export class Controller{
     private notTable: string[] = ['notfication', 'not_no'];
     private listnum : number = 19;
     private url;
-    private table : string = 'kind';
-    private func : string = '';
-    private m_get : string = '';
-    protected maintable: string[];
+    private table : string;
+    private keyField: string;
+    private func : string;
+    private m_get : string;
+    private result: string[];
 
     constructor(table: string){
         this.table = table;
 
         switch (table) {
             case this.libTable[0]:
-                this.maintable = this.libTable;
+                this.keyField = this.libTable[1];
                 break;
             case this.bookTable[0]:
-                this.maintable = this.bookTable;
+                this.keyField = this.bookTable[1];
                 break;
             case this.kindTable[0]:
-                this.maintable = this.kindTable;
+                this.keyField = this.kindTable[1];
                 break;
             case this.memTable[0]:
-                this.maintable = this.memTable;
+                this.keyField = this.memTable[1];
                 break;
             case this.matTable[0]:
-                this.matTable = this.matTable;
+                this.keyField = this.matTable[1];
                 break;
             case this.resTable[0]:
-                this.maintable = this.resTable;
+                this.keyField = this.resTable[1];
                 break;
             case this.lenTable[0]:
-                this.maintable = this.lenTable;
+                this.keyField = this.lenTable[1];
                 break;
             case this.dueTable[0]:
-                this.maintable = this.dueTable;
+                this.keyField = this.dueTable[1];
                 break;
             case this.plaTable[0]:
-                this.maintable = this.plaTable;
+                this.keyField = this.plaTable[1];
                 break;
             case this.delTable[0]:
-                this.maintable = this.delTable;
+                this.keyField = this.delTable[1];
                 break;
             case this.notTable[0]:
-                this.maintable = this.notTable;
+                this.keyField = this.notTable[1];
                 break;
             default:
                 console.log("오류 발생");
@@ -194,6 +196,11 @@ export class Controller{
         this.m_get = m_get;
     }
 
+    //result 값 가져오는 함수
+    protected getResult(){
+        return this.result;
+    }
+
     //문자가 정수인지 확인하는 함수
 	protected isInteger(strValue: string | number): boolean {
 	    return Number.isInteger(strValue);;
@@ -248,7 +255,7 @@ export class Controller{
 					max = 31;
 				}
 				else {
-					if(year%400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+					if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
 						max = 30;
 					}
 					else {
@@ -278,7 +285,7 @@ export class Controller{
 		return bool;
 	}
 
-    
+    //반납 날짜 계산 - 문자 반환
     protected estimateReturndate(lentdata: Date, extend: number): string{
         let assist = new Assistance();
         let date = assist.estimateReturndate(lentdata, extend);
@@ -287,41 +294,135 @@ export class Controller{
         return str_date;
     }
 
-    //페이그멘테이션 만들기
-    protected makePage(TableManager $mainTable, int $total_cnt, string $sql, bool $iswhere){
-        $value = '없음';
+    //검색 결과 모두 가져오기
+    protected selectAll(): string[]{
+        const sql = 'SELECT * FROM `' + this.table + '`';
+        const data = PDO(sql, '');
+        const getdata: string[] = JSON.parse(JSON.stringify(data));
+        return getdata;
+    }
 
-        if(isset($_GET['value'])){
-            $value = $_GET['value'];
+    //선택한 primary key에 대한 결과 얻기
+    protected selectID(id: number): string[]{
+        const sql = 'SELECT * FROM `' + this.table + '` WHERE `' + this.keyField + '` = ' + id;
+        const data = PDO(sql, '');
+        const getdata: string[] = JSON.parse(JSON.stringify(data));
+        return getdata;
+    }
+
+    //조인 쿼리를 사용할 때 사용하는 함수
+    protected joinSQL(sql: string): string[]{
+        const data = PDO(sql, '');
+        const getdata: string[] = JSON.parse(JSON.stringify(data));
+        return getdata;
+    }
+
+    //where절을 이용한 검색 함수
+    protected whereSQL(where: string): string[]{
+        const sql = 'SELECT * FROM `' + this.table + '` ' + where;
+        const data = PDO(sql, '');
+        const getdata: string[] = JSON.parse(JSON.stringify(data));
+        return getdata;
+    }
+
+    //커스텀 sql - delupdateSQL()
+    protected customSQL(sql: string): string[]{
+        const data = PDO(sql, '');
+        const getdata: string[] = JSON.parse(JSON.stringify(data));
+        return getdata;
+    }
+
+    protected insertData(param: Map<string, string>): void{
+        var sql = 'INSERT INTO `' + this.table + '` SET ';
+        param.forEach((values, key) => {
+            sql += '`' + key + '`= ' + values + ', ';
+        });
+        sql = sql.replace(/,\s*$/, '');
+        PDO(sql, '');
+    }
+
+    protected deleteData(id: string): void{
+        let sql = 'DELETE FROM `' + this.table + '` WHERE `' + this.keyField + '`= ' + id;
+        PDO(sql, '');
+    }
+
+    protected updateData(param: Map<string, string>): void{
+        var sql = 'UPDATE`' + this.table + '`SET ';
+        param.forEach((value, key) => {
+            sql += '`' + key + '`= ' + value + ', ';
+        });
+        sql = sql.replace(/,\s*$/, '');
+        sql += ' WHERE `' + this.keyField + '`= ' + param[this.keyField];
+        PDO(sql, '');
+    }
+
+    protected allupdataData(param: Map<string, string>, id: number): void{
+        var sql = 'UPDATE`' + this.table + '`SET ';
+        param.forEach((value, key) => {
+            sql += '`' + key + '`= ' + value + ', ';
+        });
+        sql = sql.replace(/,\s*$/, '');
+        sql += ' WHERE `' + this.keyField + '`= ' + id;
+        PDO(sql, '');
+    }
+
+    protected updateNullData(param: Map<string, string>, id: number): void{
+        var sql = 'UPDATE`' + this.table + '`SET ';
+        param.forEach((value, key) => {
+            sql += '`' + key + '`= NULL, ';
+        });
+        sql = sql.replace(/,\s*$/, '');
+        sql += ' WHERE `' + this.keyField + '`= ' + id;
+        PDO(sql, '');
+    }
+
+    //페이그멘테이션 만들기 -- 여기 부터
+    protected makePage(total_cnt: number, sql: string, iswhere: boolean): string{
+        var pagi: string;
+        var result: string[];
+        var value: string = '없음';
+
+        if(this.url.get('value') != null){
+            value = this.url.get('value');
         }
         
-        $sql = this.pagesql($sql);
+        sql = this.pagesql(sql);
 
-        if($iswhere){
-            $stmt = $mainTable->whereSQL($sql);
-            $result = $stmt->fetchAll();
-            $pagi = this.pagemanager($total_cnt, $value);
+        if(iswhere){
+            result = this.whereSQL(sql);
+            pagi = this.pagemanager(total_cnt, value);
         }else{
-            $stmt = $mainTable->joinSQL($sql);
-            $result = $stmt->fetchAll();
-            $pagi = this.pagemanager($total_cnt, $value);
+            result = this.joinSQL(sql);
+            pagi = this.pagemanager(total_cnt, value);
         }
 
-        this.result = $result;
-        return $pagi;
+        this.result = result;
+        return pagi;
     }
 
     //대출이 가능한지 확인
-    protected lentpossible(int $mem_no, TableManager $memTable, TableManager $lenTable){
-        $result = $memTable->selectID($mem_no);
-        $mem_lent = $result['mem_lent'];
+    protected lentpossible(mem_no: number): boolean{
+        let m_table: string = this.table;
+        let m_key: string = this.keyField;
+        let mem_lent: number;
+        let result: string[];
+        let where: string;
 
-        $where = "WHERE `mem_no` = $mem_no AND `len_re_date` IS NULL";
-        $rs = $lenTable->whereSQL($where);
-        $num = $rs->rowCount();
+        this.table = this.memTable[0];
+        this.keyField = this.memTable[1];
+        result = this.selectID(mem_no);
+        mem_lent = +result['mem_lent'];
 
-        if($num > $mem_lent){
-            echo "<script>alert('대출가능수를 초과했습니다.');</script>";
+        this.table = this.lenTable[0];
+        this.keyField =  this.lenTable[1];
+        where = "WHERE `mem_no` = $mem_no AND `len_re_date` IS NULL";
+        result = this.whereSQL(where);
+
+        this.table = m_table;
+        this.keyField = m_key;
+
+        if(result.length > mem_lent){
+            alert('대출가능수를 초과했습니다.');
             return false;
         }
 
@@ -329,76 +430,100 @@ export class Controller{
     }
 
     //자료가 있는지 확인
-    protected existMat(int $mat_no, int $mat_exist, TableManager $lenTable, TableManager $delTable, TableManager $matTable){
-        $bool = true;
+    protected existMat(mat_no: number, mat_exist: number): void{
+        var m_table: string = this.table;
+        var m_key: string = this.keyField;
+        var bool: boolean = true;
 
-        if($mat_exist == 1){
-            $where = "WHERE `mat_no` = $mat_no AND `len_re_st` = 0";
-            $rs = $lenTable->whereSQL($where);
-            $bool = this.resultempty_check($rs);
+        if(mat_exist == 1){
+            var where: string = "WHERE `mat_no` = $mat_no AND `len_re_st` = 0";
+            this.table = this.lenTable[0];
+            this.keyField = this.lenTable[1];
+            var rs = this.whereSQL(where);
+            bool = this.resultempty_check(rs);
 
-            if($bool){
-                $where = "WHERE `mat_no` =  $mat_no AND `del_app` = 1 AND `len_no` IS NULL";
-                $rs = $delTable->whereSQL($where);
-                $bool = this.resultempty_check($rs);
+            this.table = this.delTable[0];
+            this.keyField = this.delTable[1];
+
+            if(bool){
+                where = "WHERE `mat_no` =  $mat_no AND `del_app` = 1 AND `len_no` IS NULL";
+                rs = this.whereSQL(where);
+                bool = this.resultempty_check(rs);
             }
             
-            if($bool){
-                $where = "WHERE `mat_no` =  $mat_no AND `del_app` = 2 AND `del_arr_date` IS NULL";
-                $rs = $delTable->whereSQL($where);
-                $bool = this.resultempty_check($rs);
+            if(bool){
+                where = "WHERE `mat_no` =  $mat_no AND `del_app` = 2 AND `del_arr_date` IS NULL";
+                rs = this.whereSQL(where);
+                bool = this.resultempty_check(rs);
             }
 
-            if($bool){
-                $bool = false;
+            if(bool){
+                bool = false;
             }else{
-                $bool = true;
+                bool = true;
             }
         }
 
-        if($bool){
-            $param = ['mat_no'=>$mat_no,'mat_exist'=>$mat_exist];
-            $matTable->updateData($param);
+        if(bool){
+            let param = new Map<string, string>();
+            param.set('mat_no', mat_no.toString());
+            param.set('mat_exist', mat_exist.toString());
+            this.table = this.matTable[0];
+            this.keyField = this.matTable[1];
+            this.updateData(param);
         }
+
+        this.table = m_table;
+        this.keyField = m_key;
     }
 
     //예약도서인지 확인 만약에 예약도서이면 현재 회원키와 예약도서 예약된 회원키를 같으면 대출 아니면 대출 거절
-    protected reservationCheck(int $res_no, int $mat_no, TableManager $resTable){
-        $rs = false;
+    protected reservationCheck(res_no: string, mat_no: string, mem_no: string): boolean{
+        var num: number;
+        var where: string;
+        var row: string[];
+        var rs: boolean = false;
+        var m_table: string = this.table;
+        var m_key: string = this.keyField;
 
-        if($res_no == ''){
-            $where = "WHERE `mat_no` = $mat_no";
-            $stmt = $resTable->whereSQL($where);
-            $num = $stmt->rowCount();
+        this.table = this.resTable[0];
+        this.keyField = this.resTable[1];
+
+        if(res_no == null){
+            where = "WHERE `mat_no` = " + mat_no;
+            row = this.whereSQL(where);
+            num = row.length;
             
-            if($num == 0){
-                $rs = true;
+            if(num == 0){
+                rs = true;
             }
             else{
-                $row = $stmt->fetch();
-                if($row['mem_no'] == $_POST['mem_no']){
-                    $rs = true;
-                    $res_no = $row['res_no'];
+                if(row['mem_no'] == mem_no){
+                    rs = true;
+                    res_no = row['res_no'];
                 }
             }
         }
         else{
-            $row = $resTable->selectID($res_no);//
+            row = this.selectID(+res_no);//
 
-            if($row['mem_no'] == $_POST['mem_no']){
-                $rs = true;
+            if(row['mem_no'] == mem_no){
+                rs = true;
             }
             else{
-                $rs = false;
+                rs = false;
             }
         }
         
-        if($rs == false){
-            echo "<script>alert('다른 회원이 예약한 도서입니다.');</script>";
+        if(rs == false){
+            alert('다른 회원이 예약한 도서입니다.');
         }else{
-            $resTable->deleteData($res_no);
+            this.deleteData(res_no);
         }
 
-        return $rs;
+        this.table = m_table;
+        this.keyField = m_key;
+
+        return rs;
     }
 }
