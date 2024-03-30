aac7298054d9534c64f10fc83a9cb9b5fb39115a
Hwangtaehun <r283@naver.com>
2024-03-31 01:37:54 +0900

    업로드

--- next-version/app/page.tsx
+++ next-version/app/page.tsx
@@ -1,6 +1,18 @@
-import PDO from "../components/includes/Dbconnect"
+import { handleLogin } from "./action";
+
+async function server(data){
+    'use server';
+    handleLogin(data);
+}
 
 export default async function home(){
+    const data = {
+        id: '1234',
+        name: 'test',
+        state: '1',
+    };
+
+    server(data);
     
     return <h1>Hello NextJs</h1>
 }
