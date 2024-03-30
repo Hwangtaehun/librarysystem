017d32ec4395201740e0e7a9ce095c09f46d4884
Hwangtaehun <r283@naver.com>
2024-03-31 00:45:32 +0900

    업로드

---
+++ next-version/components/includes/navigation.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+export default function Navigation(){
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/about-us">About Us</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
