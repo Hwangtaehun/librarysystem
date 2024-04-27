76acfba609375fbc7514defaefd2122c009d1abd
Hwangtaehun <r283@naver.com>
2024-04-27 19:36:24 +0900

    Update page.tsx

--- next-version/app/(home)/page.tsx
+++ next-version/app/(home)/page.tsx
@@ -148,14 +148,27 @@ export default async function home(props){
     async function holiday(m_year: string, m_month: string){
         let holiday = [];
 
+        if(Number(m_month) < 10){
+            m_month = '0' + m_month;
+        }
+
         var url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo'; /*URL*/
-        var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + process.env.PUBLIC_PW; /*Service Key*/
+        var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + process.env.NEXT_PUBLIC_PUBLIC_PW; /*Service Key*/
         queryParams += '&' + encodeURIComponent('solYear') + '=' + encodeURIComponent(m_year); /**/
         queryParams += '&' + encodeURIComponent('solMonth') + '=' + encodeURIComponent(m_month); /**/
         let result = await fetch(url+queryParams);
         console.log(url+queryParams);
-        const xmlData = await result.text();
-        console.log(xmlData);
+        let str = await result.text();
+        let index = 0;
+
+        while(str.indexOf('<locdate>', index) != -1){
+            index = str.indexOf('<locdate>', index) + 9;
+            let date = str.slice(index, index + 8); 
+            let price = Number(date.slice(6, 8));
+            holiday.push(price);
+        }
+
+        return holiday;
     }
 
     if(state != '1'){
@@ -181,8 +194,6 @@ export default async function home(props){
             not_board.push(<div id="div_row"><a className="http" href={m_url}>{cnt} {result[i]['not_name']} {m_date}</a></div>);
             cnt++;
         }
-
-        holiday('2024', '04');
 
         return (
             <>
